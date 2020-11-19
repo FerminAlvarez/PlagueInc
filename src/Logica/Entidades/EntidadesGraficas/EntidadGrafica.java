22dbf32a41884af0c5383dff34d5fb68287a63ae
Nicolas <nicomleidi@gmail.com>
2020-11-19 14:32:45 -0300

    Soldado chiquito

--- src/Logica/Entidades/EntidadesGraficas/EntidadGrafica.java
+++ src/Logica/Entidades/EntidadesGraficas/EntidadGrafica.java
@@ -29,7 +29,7 @@ public abstract class EntidadGrafica {
 	
 	public void cambiarImagen(String s) {
 		ImageIcon ii = new ImageIcon(s);
-        imagen = ii.getImage().getScaledInstance(50, 50,  java.awt.Image.SCALE_SMOOTH);
+        imagen = ii.getImage().getScaledInstance(ancho, alto,  java.awt.Image.SCALE_SMOOTH);
 	}
 	
 	public Image getImagen() {
--- src/Logica/Entidades/EntidadesGraficas/JugadorGrafica.java
+++ src/Logica/Entidades/EntidadesGraficas/JugadorGrafica.java
@@ -8,12 +8,14 @@ import javax.imageio.ImageIO;
 
 public class JugadorGrafica extends EntidadGrafica{
 
+	protected int alto = 50, ancho = 50;
+	
 	public JugadorGrafica(int x, int y) {
 		super(x, y);
 		// TODO Auto-generated constructor stub
 		InputStream in = JugadorGrafica.class.getClassLoader().getResourceAsStream("imagenes/Entidades/jugador.png");
 		try {
-			imagen = ImageIO.read(in).getScaledInstance(50, 50,  java.awt.Image.SCALE_SMOOTH);
+			imagen = ImageIO.read(in).getScaledInstance(ancho, alto,  java.awt.Image.SCALE_SMOOTH);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
