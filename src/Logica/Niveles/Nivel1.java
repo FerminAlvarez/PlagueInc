567819d0450815e331ff09c009a240ef0c85bbff
Fermin Alvarez <feeralvarez013@gmail.com>
2020-11-18 21:38:03 -0300

    Creación del fondo para el primer nivel.

--- src/Logica/Niveles/Nivel1.java
+++ src/Logica/Niveles/Nivel1.java
@@ -1,5 +1,17 @@
 package Logica.Niveles;
 
 public class Nivel1 extends EstadoNivel{
+	
+	private String fondo = "C:\\Users\\Fermin\\Desktop\\Proyecto JUEGO TDP\\pr\\src\\imagenes\\Fondo2.png";
+	
+	
+	public String obtenerFondo() {
+		return fondo;
+	}
 
+	@Override
+	protected void empezar() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/Logica/Niveles/Nivel3.java
+++ src/Logica/Niveles/Nivel3.java
@@ -2,4 +2,16 @@ package Logica.Niveles;
 
 public class Nivel3 extends EstadoNivel{
 
+	@Override
+	protected void empezar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String obtenerFondo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
